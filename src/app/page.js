c5f8c98ad20d499738b224a8f884f45c097ed980
yaghoubi-m <yaghoubifrontdev@gamil.com>
2023-10-11 21:23:44 +0330

    added Customers section

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
-"use client";
+'use client';
 import Image360Deg from '@/components/360image/360Image';
 import Header from '@/components/Header/Header';
 import AboutUs from '@/components/Home/AboutUs/AboutUS';
+import SampleProjects from '@/components/Home/SampleProjects/SampleProjects';
+import Customers from '@/components/Home/customers/Customers';
 
 export default function Home() {
   return (
@@ -9,6 +11,8 @@ export default function Home() {
       <Header />
       <AboutUs />
       <Image360Deg />
+      <SampleProjects />
+      <Customers />
     </main>
   );
 }
